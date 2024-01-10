c8885e30b67dbe14ff57e60064d521392a121b95
Prathamesh18X <109477390+Prathamesh18X@users.noreply.github.com>
2024-01-10 15:30:34 +0530

    Added User Routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const pizzaModel = require('./pizzaModel');
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,5 +12,22 @@ router.post("/register", async(req,res) => {
         return res.status(400).json({message: error})
     }
 })
+router.post("/login", async(req,res) => {
+    const {email , password} = req.body
+    try {
+        const user = await Users.find({email, password})
+        if(user.length > 0){
+            const currentUser ={
+                name : user[0].name,
+                email : user[0].email,
+                isAdmin : user[0].isAdmin,
+                _id : user[0]._id,
+            }
+            res.send(currentUser);
+        }
+    } catch (error) {
+        return res.status(400).json({message: error})
+    }
+})
 
 module.exports = router;
