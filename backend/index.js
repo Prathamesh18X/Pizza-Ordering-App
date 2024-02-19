7561978c3c295670024d0e35d795aac295fd3bea
Prathamesh18X <109477390+Prathamesh18X@users.noreply.github.com>
2024-02-20 01:04:03 +0530

    Server Deployment

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,17 @@ const db = require('./pizzaDB');
 const app = express();
 const pizzasRoute = require('./routes/pizzaRoutes')
 const userRoute = require('./routes/userRoutes')
-const port = process.env.PORT || 8000;
+const port = 8000
 
-//plugins
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
 
-//testing
+
 app.get("/", (req, res) => {
   res.send("Server working with status 200");
 });
 
 
-//Routes
+
 app.use('/api/pizzas/' , pizzasRoute)
 app.use('/api/users/' , userRoute)
 
