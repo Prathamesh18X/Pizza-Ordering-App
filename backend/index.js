444a6a4b776f1a3f8748b445a1fec9887414b0db
Prathamesh18X <109477390+Prathamesh18X@users.noreply.github.com>
2024-02-20 13:59:55 +0530

    v1.0.1

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,29 @@
 const express = require("express");
+const cors = require('cors');
 const db = require('./pizzaDB');
 const app = express();
 const pizzasRoute = require('./routes/pizzaRoutes')
 const userRoute = require('./routes/userRoutes')
 const port = 8000
 
+
+//Plugin middlewares
+app.use(cors());
 app.use(express.json());
 
+// Restrict CORS to a specific origin
+const corsOptions = {
+  origin: 'https://pizzas-store.vercel.app',
+};
 
+app.use(cors(corsOptions));
+//testing
 app.get("/", (req, res) => {
   res.send("Server working with status 200");
 });
 
 
-
+//All routes
 app.use('/api/pizzas/' , pizzasRoute)
 app.use('/api/users/' , userRoute)
 
