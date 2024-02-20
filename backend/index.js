dd7fab5b7154d0c5146c7497e170342f2d806c4b
Prathamesh18X <109477390+Prathamesh18X@users.noreply.github.com>
2024-02-20 15:00:40 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,27 @@ const express = require("express");
 const cors = require('cors');
 const db = require('./pizzaDB');
 const app = express();
-const pizzasRoute = require('./routes/pizzaRoutes');
-const userRoute = require('./routes/userRoutes');
-const port = 8000;
+const pizzasRoute = require('./routes/pizzaRoutes')
+const userRoute = require('./routes/userRoutes')
+const port = 8000
 
-// Enable CORS for all routes
+
+//Plugin middlewares
 app.use(cors());
-
-// Parse JSON request body
 app.use(express.json());
 
-// Define a route for testing
+app.use(cors(corsOptions));
+//testing
 app.get("/", (req, res) => {
   res.send("Server working with status 200");
 });
 
-// Routes for pizzas and users
-app.use('/api/pizzas/', pizzasRoute);
-app.use('/api/users/', userRoute);
+
+//All routes
+app.use('/api/pizzas/' , pizzasRoute)
+app.use('/api/users/' , userRoute)
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
